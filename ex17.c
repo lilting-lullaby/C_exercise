4b5a33052dc35eee6640a95679521f27d67e0000
He Wang <brank12138@gmail.com>
2024-11-08 15:45:46 +0800

    I finish the second question of charpter 17

--- ex17.c
+++ ex17.c
@@ -44,14 +44,14 @@ void Database_load(struct Connection *conn)
 {
 	int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
 	if(rc != 1) die("Failed to load database.");
-        printf("%d\n",conn->db->MAX_ROWS);
+        // printf("%d\n",conn->db->MAX_ROWS);
 	conn->db->rows = (struct Address*)malloc(sizeof(struct Address)*conn->db->MAX_ROWS);
 	for(size_t i = 0 ; i < conn->db->MAX_ROWS;i++){
 		rc = fread(&(conn->db->rows[i].id), sizeof(int), 1, conn->file);
-		printf("%d\n",conn->db->rows[i].id);
+	//	printf("%d\n",conn->db->rows[i].id);
 		if(rc != 1) die("Failed to load id.");
 		rc = fread(&(conn->db->rows[i].set), sizeof(int), 1, conn->file);
-		printf("%d\n",conn->db->rows[i].set);
+	//	printf("%d\n",conn->db->rows[i].set);
 		if(rc != 1) die("Failed to load set.");
 		conn->db->rows[i].name = (char*)malloc(conn->db->MAX_DATA);
 		conn->db->rows[i].email = (char*)malloc(conn->db->MAX_DATA);
@@ -96,24 +96,12 @@ void Database_close(struct Connection *conn)
 
 void Database_write(struct Connection *conn)
 {
-    rewind(conn->file);
+    rewind(conn->file);//  reset the conn->file to the begining of the file . 
 
     int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
     if(rc != 1) die("Failed to write database.");
-//    for (int i = 0 ; i < conn->db->MAX_ROWS ; i ++){
-//   
-//    rc = fwrite(&(conn->db->rows[i].id), sizeof(int), 1, conn->file);
-//    if(rc != 1) die("Failed to write ID.");
-//
-//    rc = fwrite(&(conn->db->rows[i].set), sizeof(int), 1, conn->file);
-//    if(rc != 1) die("Failed to write set.");
-//    rc = fwrite(conn->db->rows[i].name, sizeof(conn->db->MAX_DATA*sizeof(char)), 1, conn->file);
-//    if(rc != 1) die("Failed to write name.");
-//    rc = fwrite(conn->db->rows[i].email, sizeof(conn->db->MAX_DATA*sizeof(char)), 1, conn->file);
-//    if(rc != 1) die("Failed to write email.");
-//    }
-//
-     for (size_t i = 0; i < conn->db->MAX_ROWS; i++) {
+     
+    for (size_t i = 0; i < conn->db->MAX_ROWS; i++) {
          if(fwrite(&((conn->db->rows[i]).id), sizeof(int), 1, conn->file) != 1)
               die("Failed to write id.");
          if(fwrite(&((conn->db->rows[i]).set), sizeof(int), 1, conn->file) != 1)
@@ -123,7 +111,7 @@ void Database_write(struct Connection *conn)
          if(fwrite((conn->db->rows[i]).email, conn->db->MAX_DATA, 1, conn->file) != 1)
               die("Failed to write email.");
      }
-    rc = fflush(conn->file);
+    rc = fflush(conn->file);// tell the computer that wirte the data to the disk immedately.
     if(rc == -1) die("Cannot flush database.");
 }
 
@@ -139,16 +127,16 @@ void Database_create(struct Connection *conn,int MAX_ROWS,int MAX_DATA)
         struct Address addr = {.id = i, .set = 0};
 	addr.name = (char*)malloc(MAX_DATA*sizeof(char));
 	addr.email = (char*)malloc(MAX_DATA*sizeof(char));
-        printf("id:%d set:%d\n",addr.id,addr.set);
-	memset(addr.name, 0, conn->db->MAX_DATA);
+        //printf("id:%d set:%d\n",addr.id,addr.set);
+	memset(addr.name, 0, conn->db->MAX_DATA); //memeset : initailize the memory you alloc , it must be done to avoid SG11.
 	memset(addr.email, 0, conn->db->MAX_DATA);
 
-        printf("id:%s set:%s\n",addr.name,addr.email);
+        //printf("id:%s set:%s\n",addr.name,addr.email);
 
         // then just assign it
         conn->db->rows[i] = addr;
         
-        printf("id:%d set:%d\n",conn->db->rows[i].id,conn->db->rows[i].set);
+        //printf("id:%d set:%d\n",conn->db->rows[i].id,conn->db->rows[i].set);
     }
 }
 
@@ -156,7 +144,7 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
 {
     struct Address *addr = &conn->db->rows[id];
 
-    printf("%s,%s\n",name,email);
+    //printf("%s,%s\n",name,email);
     if(addr->set) die("Already set, delete it first");
     addr->name = (char*)malloc(conn->db->MAX_DATA);
     addr->email = (char*)malloc(conn->db->MAX_DATA);
@@ -165,7 +153,7 @@ void Database_set(struct Connection *conn, int id, const char *name, const char 
     char *res = strncpy(addr->name, name,conn->db->MAX_DATA);
     // demonstrate the strncpy bug
     if(!res) die("Name copy failed");
-    printf("%s,%s\n",conn->db->rows[id].name,addr->name);
+    //printf("%s,%s\n",conn->db->rows[id].name,addr->name);
     res = strncpy(addr->email, email,conn->db->MAX_DATA);
     if(!res) die("Email copy failed");
 }
@@ -185,7 +173,7 @@ void Database_delete(struct Connection *conn, int id)
 {
     struct Address addr = {.id = id, .set = 0};
     conn->db->rows[id] = addr;
-    conn->db->rows[id].name = (char*)malloc(conn->db->MAX_DATA);
+    conn->db->rows[id].name = (char*)malloc(conn->db->MAX_DATA);//you have to malloc the memory & initialize it .
     conn->db->rows[id].email = (char*)malloc(conn->db->MAX_DATA);
     memset(conn->db->rows[id].name, 0, conn->db->MAX_DATA);
     memset(conn->db->rows[id].email, 0, conn->db->MAX_DATA);
@@ -199,7 +187,7 @@ void Database_list(struct Connection *conn)
     int i = 0;
     struct Database *db = conn->db;
 
-    for(i = 0; i <db->MAX_ROWS; i++) {
+    for(i = 0; i < db->MAX_ROWS; i++) {
         struct Address *cur = &db->rows[i];
 
         if(cur->set) {
